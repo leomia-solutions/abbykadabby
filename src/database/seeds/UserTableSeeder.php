47cdd95f77c1853530adb9691ca476c4f8c34805
Matt Campo <matt.campo@leomiasolutions.com>
2019-08-05 15:52:49 -0400

    Added database seeder for User class

---
+++ src/database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+        User::create([
+            'first_name' => 'Matt',
+            'last_name' => 'Campo',
+            'email'    => 'matt.campo@leomiasolutions.com',
+            'password' => 'password',
+        ]);
+    }
+}
