c2c557db484320b5555a51e5a541b96b17126a79
Matt Campo <matt.campo@leomiasolutions.com>
2019-08-06 17:48:04 -0400

    Validation messages now display on the edit form

--- src/resources/views/inventory/edit.blade.php
+++ src/resources/views/inventory/edit.blade.php
@@ -7,18 +7,30 @@
 <form method="POST" action="{{ route('inventoryUpdate', $record->id) }}">
 	@csrf
 
+	@if ($errors->has('description'))
+	<div class="error">{{ $errors->first('description') }}</div>
+	@endif
 	<label for="description">Description:</label>
 	<input type="text" name="description" value="{{ $record->description }}" />
 	<br />
 
+	@if ($errors->has('quantity'))
+	<div class="error">{{ $errors->first('quantity') }}</div>
+	@endif
 	<label for="quantity">Quantity:</label>
 	<input type="number" name="quantity" value="{{ $record->quantity }}" />
 	<br />
 
+	@if ($errors->has('weight'))
+	<div class="error">{{ $errors->first('weight') }}</div>
+	@endif
 	<label for="weight">Weight:</label>
 	<input type="text" name="weight" value="{{ $record->weight }}" />
 	<br />
 
+	@if ($errors->has('weight_units'))
+	<div class="error">{{ $errors->first('weight_units') }}</div>
+	@endif
 	<label for="weight_units">Price Units:</label>
 	<select name="weight_units">
 		<option value="">Select</option>
@@ -27,10 +39,16 @@
 	</select>
 	<br />
 
+	@if ($errors->has('price'))
+	<div class="error">{{ $errors->first('price') }}</div>
+	@endif
 	<label for="price">Price:</label>
 	<input type="text" name="price" value="{{ $record->price }}" />
 	<br />
 
+	@if ($errors->has('price_units'))
+	<div class="error">{{ $errors->first('price_units') }}</div>
+	@endif
 	<label for="price_units">Price Units:</label>
 	<select name="price_units">
 		<option value="">Select</option>
